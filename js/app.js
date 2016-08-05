8717009ea5e98b0224c90bc3a297b86ece0339e7
李杨帆 <2531349593@qq.com>
2016-08-05 09:51:54 +0800

    add rightpanel

--- js/app.js
+++ js/app.js
@@ -8,13 +8,20 @@ const  App= React.createClass({
     toggle(){
       this.setState({isEditor:!this.state.isEditor})
     },
+    addCount(count){
+        const counts = this.state.counts;
+        counts.push(count);
+        this.setState({counts});
+
+        console.log(counts)
+    },
     render(){
         const isEditor=this.state.isEditor;
         return <div>
             <button onClick={this.toggle}>{isEditor?"预览":"编辑"}</button>
             <div>
                 <Left></Left>
-                <Right></Right>
+                <Right onAdd={this.addCount}></Right>
             </div>
             <div>
                 <Preview></Preview>
@@ -27,11 +34,20 @@ const Left = React.createClass({
     render(){
         return <div></div>
     }
+
 });
 
 const Right = React.createClass({
+    add(){
+        const count = $("input[name=input]:checked").val();
+        this.props.onAdd(count);
+    },
     render(){
-        return <div></div>
+        return <div>
+            <input type="radio" name="input" value="text"/>Text
+            <input type="radio" name="input" value="date"/>Date
+            <button onClick={this.add}>+</button>
+        </div>
     }
 });
 
