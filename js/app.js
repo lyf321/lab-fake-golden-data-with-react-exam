9cafd45f51a873d065625ce28287a7cc8c778652
李杨帆 <2531349593@qq.com>
2016-08-05 09:47:49 +0800

    exchange page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const App = React.createClass({
+const  App= React.createClass({
     getInitialState(){
       return{
           isEditor:true,
@@ -6,78 +6,39 @@ const App = React.createClass({
       }
     },
     toggle(){
-      this.setState({isEditor:!this.state.isEditor});
-    },
-    addCount(count){
-        const counts = this.state.counts;
-        counts.push(count);
-        this.setState({counts})
-    },
-    deleteCount(index){
-        const counts = this.state.counts;
-        counts.splice(index);
-        this.setState({counts})
+      this.setState({isEditor:!this.state.isEditor})
     },
     render(){
         const isEditor=this.state.isEditor;
         return <div>
             <button onClick={this.toggle}>{isEditor?"预览":"编辑"}</button>
-            <div className={isEditor?"":"hidden"}>
-                <Left counts={this.state.counts} onDelete={this.deleteCount}></Left>
-                <Right onAdd={this.addCount}></Right>
+            <div>
+                <Left></Left>
+                <Right></Right>
             </div>
-            <div className={isEditor?"hidden":""}>
-                <Preview counts={this.state.counts} ></Preview>
+            <div>
+                <Preview></Preview>
             </div>
         </div>
     }
 });
 
 const Left = React.createClass({
-    remove(index){
-        this.props.onDelete(index);
-    },
     render(){
-        const counts = this.props.counts.map((count,index)=>{
-            return <div key={index}>
-                <input type={count}/>
-                <button onClick={this.remove.bind(this,index)}>X</button>
-            </div>
-        })
-        return <div>
-            {counts}
-        </div>
+        return <div></div>
     }
-
 });
 
 const Right = React.createClass({
-    add(){
-        const count = $("input[name=input]:checked").val()
-        this.props.onAdd(count)
-    },
     render(){
-        return <div>
-            <input type="radio" name="input" value="text"/>Text
-            <input type="radio" name="input" value="date"/>Date
-            <button onClick={this.add}>+</button>
-        </div>
+        return <div></div>
     }
 });
 
 const Preview = React.createClass({
     render(){
-        const counts = this.props.counts.map((count,index)=>{
-            return <div key={index}>
-                <input type={count}/>
-            </div>
-        })
-        return <div>
-            {counts}
-            <button>提交</button>
-        </div>
+        return <div></div>
     }
 });
-
 
 ReactDOM.render(<App/>,document.getElementById('content'))
